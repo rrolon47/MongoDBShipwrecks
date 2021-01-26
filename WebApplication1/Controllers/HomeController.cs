56f7963ae0bec503d79075317ee32a1e879df44b
Rachel Rolon <66883810+rrolon47@users.noreply.github.com>
2021-01-25 20:22:46 -0600

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IMongoCollection<Shipwreck> _shipwreckCollection;
+
+        public HomeController(IMongoClient client)
+        {
+            //create collection 
+            
+            //points to atlas cluster 
+            var database = client.GetDatabase("sample_geospatial");
+            //points to what collection we want
+            _shipwreckCollection = database.GetCollection<Shipwreck>("shipwrecks"); /*<bsondocument> is any bson data type, but here we specified*/
+        }
+
+        public IActionResult Index()
+        {
+            List<Shipwreck> shipwrecksVisble = _shipwreckCollection.Find(s => s.FeatureType == "Wrecks - Visible").ToList();
+            return View(shipwrecksVisble);
+        }
+
+    
+        //[HttpGet]
+        //public IEnumerable<Shipwreck> Get()
+        //{ 
+        //    //search for data in collection collection.Find(...)
+        //    return _shipwreckCollection.Find(s => s.FeatureType == "Wrecks - Visible").ToList();
+        //}
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication1/Models/Shipwreck.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    [BsonIgnoreExtraElements]
+    //^ that tells the program to just ignore any properties that arent identified 
+    public class Shipwreck
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("feature_type")]
+        public string FeatureType { get; set; }
+
+        [BsonElement("chart")]
+        public string Chart { get; set; }
+
+        [BsonElement("latdec")]
+        public double Lat { get; set; }
+
+        [BsonElement("londec")]
+        public double Long { get; set; }
+
+        //This acts as a container for data that we are not using but it will take up processing data
+        //[BsonExtraElements]
+        //public object[] Bucket { get; set; }
+
+    }
+}
